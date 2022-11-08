134bc9a51e91174abe5f125961c9e3fe336fc5e6
PremVispute <Premvispute@gmail.com>
2022-11-08 12:23:34 +0530

    sidebar with animations

--- src/components/layouts/SideBar/SideBarMain.js
+++ src/components/layouts/SideBar/SideBarMain.js
@@ -9,23 +9,25 @@ import { useState } from 'react'
 
 const SideBarMain = () => {
   const [sidebar, setSideBar] = useState(0)
+  const [div, setDiv] = useState("div2")
 
   const toggleDiv = (id) => {
     let sideBarSecondaryData = SideBarSecondaryData.find(data => { return data.id === id })
     setSideBar(sideBarSecondaryData)
+    div === "div1" ? setDiv("div2") : setDiv("div1")
   }
 
   return (
     <>
-    {sidebar && sidebar.id ? <SideBarSecondary data={sidebar}/> : null}   
-    <div className='sidebar-main'>
+    {sidebar && sidebar.id ? <SideBarSecondary data={sidebar} id="div1" className={div === "div1" ? "div1 show" : "div1 hide"}/> : null}   
+    <div id="div2" className={div === "div2" ? "div2 show sidebar-main" : "div2 hide sidebar-main"}>
       <ListGroup defaultActiveKey="/">
         {SideBarMainData.map(SideBarMainData => (
           <div key={SideBarMainData.id}>            
           <Link className='link' onClick={() => toggleDiv(SideBarMainData.id)} to={SideBarMainData.link}>
             <ListGroup.Item variant='dark' key={SideBarMainData.id} action href={SideBarMainData.link}>
               <FontAwesomeIcon className='me-3' icon={SideBarMainData.icon} />
-              <span className='sidebar-text'>
+              <span className={div === "div1" ? "sidebar-text" : ""}>
                 {SideBarMainData.label}
               </span>
             </ListGroup.Item>
